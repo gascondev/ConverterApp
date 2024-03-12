f53d7a22f9cbaebe3a9607f6bb91cc384f5656e1
Álvaro Gascón <alvarogascon@hotmail.com>
2024-03-12 12:11:00 +0100

    code refactoring

--- ConverterApp/ContentView.swift
+++ ConverterApp/ContentView.swift
@@ -9,81 +9,84 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var value1: Double = 0.0
-    @State private var fromIndex = 0
-    @State private var toIndex = 0
-
-    let converter = ["Celsius (ºC)", "Fharenheit (ºF)", "Kelvin (ºK)"]
+    @State private var input = 100.0
+    @State private var inputUnit: Dimension = UnitLength.meters
+    @State private var outputUnit: Dimension = UnitLength.kilometers
+    @State private var selectedUnits = 0
     
-    var convertedValue: Double {
-        let inputValue = value1
+    @FocusState private var inputIsFocused: Bool
     
-        switch (fromIndex, toIndex) {
-        case (0, 0): // Celsius a Celsius
-            return inputValue
-        case (0, 1): // Celsius a Fahrenheit
-            return (inputValue * 9/5) + 32
-        case (0, 2): // Celsius a Kelvin
-            return inputValue + 273.15
-        case (1, 0): // Fahrenheit a Celsius
-            return (inputValue - 32) * 5/9
-        case (1, 1): // Fahrenheit a Fahrenheit
-            return inputValue
-        case (1, 2): // Fahrenheit a Kelvin
-            return (inputValue + 459.67) * 5/9
-        case (2, 0): // Kelvin a Celsius
-            return inputValue - 273.15
-        case (2, 1): // Kelvin a Fahrenheit
-            return (inputValue * 9/5) - 459.67
-        case (2, 2): // Kelvin a Kelvin
-            return inputValue
-        default:
-            return 0.0
-        }
+    let conversions = ["Distance", "Mass", "Temperature", "Time"]
+    
+    let unitTypes = [
+        [UnitLength.feet, UnitLength.kilometers, UnitLength.meters, UnitLength.miles, UnitLength.yards],
+        [UnitMass.grams, UnitMass.kilograms, UnitMass.ounces, UnitMass.pounds],
+        [UnitTemperature.celsius, UnitTemperature.fahrenheit, UnitTemperature.kelvin],
+        [UnitDuration.hours, UnitDuration.minutes, UnitDuration.seconds]
+    ]
+    
+    let formatter: MeasurementFormatter
+    
+    var result: String {
+        let inputMeasurement = Measurement(value: input, unit: inputUnit)
+        let outputMeasurement = inputMeasurement.converted(to: outputUnit)
+        return formatter.string(from: outputMeasurement)
     }
-  
+    
     
     var body: some View {
-            NavigationView {
-                Form {
-                    Section(header: Text("De:")) {
-                        Picker(selection: $fromIndex, label: Text("")) {
-                            ForEach(0..<converter.count, id: \.self) {
-                                Text(self.converter[$0])
-                            }
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
-                    }
-                    
-                    Section(header: Text("A:")) {
-                        Picker(selection: $toIndex, label: Text("")) {
-                            ForEach(0..<converter.count, id: \.self) {
-                                Text(self.converter[$0])
-                            }
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
-                    }
-                    
-                    Section(header: Text("Grados \(converter[fromIndex])")) {
-                        TextField("Grados", text: Binding(
-                            get: { "\(self.value1)" },
-                            set: {
-                                if let newValue = NumberFormatter().number(from: $0) {
-                                    self.value1 = newValue.doubleValue
-                                }
-                            })
-                        )
-                        
-                    }
-                    
-                    Section(header: Text("Grados \(converter[toIndex])")) {
-                        Text("\(String(format: "%.2f", convertedValue))")
+        NavigationStack {
+            Form {
+                Section("Amount to convert") {
+                    TextField("Amount", value: $input, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputIsFocused)
+                }
+                
+                Picker("Conversion", selection: $selectedUnits) {
+                    ForEach(0..<conversions.count, id:\.self) {
+                        Text(conversions[$0])
                     }
                 }
-                .navigationTitle("Conversor")
+                
+                Picker("Convert from", selection: $inputUnit) {
+                    ForEach(unitTypes[selectedUnits], id: \.self) {
+                        Text(formatter.string(from: $0).capitalized)
+                    }
+                }
+                
+                Picker("Convert to", selection: $outputUnit) {
+                    ForEach(unitTypes[selectedUnits], id: \.self) {
+                        Text(formatter.string(from: $0).capitalized)
+                    }
+                }
+                
+                Section("Result") {
+                    Text(result)
+                }
+            }
+            .navigationTitle("Conversor")
+            .toolbar {
+                if inputIsFocused {
+                    Button("Done") {
+                        inputIsFocused = false
+                    }
+                }
+            }
+            .onChange(of: selectedUnits) {
+                let units = unitTypes[selectedUnits]
+                inputUnit = units[0]
+                outputUnit = units[1]
             }
         }
     }
+    
+    init() {
+        formatter = MeasurementFormatter()
+        formatter.unitOptions = .providedUnit
+        formatter.unitStyle = .long
+    }
+}
 
 #Preview {
     ContentView()
